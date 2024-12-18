4d4ffd256e8b41e4cf822c7d6d885f3191c5205e
Ketan Shinde <ketan_shinde@hotmail.com>
2024-12-18 18:01:59 +0530

    mongoDb Connection Added

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = 4001;
 const urlroute =require('./routes/url')
+const {ConnectToMongoDb}= require('./connections')
 
 
+ConnectToMongoDb('mongodb://localhost:27017/Url_Generator')
+.then(()=>console.log("MongoDb Connected!!!"))
 
 app.use('/url',urlroute);
 app.listen(PORT, ()=>console.log(`Server Connected On PORT :${PORT}`));
