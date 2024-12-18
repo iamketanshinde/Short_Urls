0f3f37a41096404a1b9ba5da9669cced0819377a
Ketan Shinde <ketan_shinde@hotmail.com>
2024-12-18 17:55:19 +0530

    routeUrl Added

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const PORT = 4001;
+const urlroute =require('./routes/url')
 
 
 
+app.use('/url',urlroute);
 app.listen(PORT, ()=>console.log(`Server Connected On PORT :${PORT}`));
 
 
