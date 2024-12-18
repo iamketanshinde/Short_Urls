fd46f341a1f538dfd3b4c622cde50ccf3adbad56
Ketan Shinde <ketan_shinde@hotmail.com>
2024-12-18 19:30:39 +0530

    allset

--- controllers/url.js
+++ controllers/url.js
@@ -1,17 +1,24 @@
-const shortid = require('shortid');
-const URL = require('../models/urls')
+const express = require('express');
+const shortid= require('shortid');
+const app = express();
+app.use(express.json()); 
 
-async function handleNewShortUrl(req, res){
-    const body = req.body;
-    if(!body.url) return res.status(404).json({error:"Create New Url's"})
-        const shortId = shortid();
-        await URL.create({
-            shortId:shortId,
-            redirectUrl:body.url,
-            visitHistory:[],
-        });
-        return res.json({id:shortId})
-};
+async function handleNewShortUrl(req, res) {
+  const body = req.body;
+  if (!body || !body.url) {
+    return res.status(404).json({ error: "Create New Url's" });
+  }
+  const shortId =shortid();
+  await URL.create({
+    shortId: shortId,
+    redirectUrl: body.url,
+    visitHistory: [],
+  });
+  return res.json({ id: shortId });
+}
+
+
+
 
 module.exports={
     handleNewShortUrl,
--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const {ConnectToMongoDb}= require('./connections')
 ConnectToMongoDb('mongodb://localhost:27017/Url_Generator')
 .then(()=>console.log("MongoDb Connected!!!"))
 
+
+app.use(express.json());
 app.use('/url',urlroute);
 app.listen(PORT, ()=>console.log(`Server Connected On PORT :${PORT}`));
 
