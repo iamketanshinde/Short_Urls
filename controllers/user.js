2745b70013d40799b03cc5a42981439adbcbfb3c
Ketan Shinde <ketan_shinde@hotmail.com>
2024-12-24 22:21:37 +0530

    handleSignUpUser function created

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,15 @@
+const User = require('../models/user');
+const { handleGetAnalytics } = require('./url');
+
 async function HandleSignUpUser(req, res ){
-    const body = req.body;
+    const {name, email, password} = req.body;
+    await User.create({
+        name,
+        email,
+        password,
+    })
+      return res.render('User')
+}
+module.exports = {
+    HandleSignUpUser,
 }
